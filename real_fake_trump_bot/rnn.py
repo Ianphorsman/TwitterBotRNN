7d99937ff511448ba949efa3e3281bc430bcb543
Ian Horsman <ianphorsman@gmail.com>
2018-01-23 01:54:00 -0500

    Added tensorflow session

--- real_fake_trump_bot/rnn.py
+++ real_fake_trump_bot/rnn.py
@@ -14,7 +14,8 @@ class RNN(Preprocessor):
             iterations=1000,
             batch_size=128,
             inspect_rate=50,
-            hidden_layers=(16,8)
+            hidden_layers=(16,8),
+            tweet_size=140
     ):
         # initialize Preprocessor with DataFrame of tweets / twitter data
         super().__init__(tweets=tweets)
@@ -25,9 +26,10 @@ class RNN(Preprocessor):
         self.batch_size = batch_size
         self.inspect_rate = inspect_rate
         self.hidden_layers = hidden_layers
+        self.tweet_size = tweet_size
 
-        # initialize placeholders for input and output
-        self.X = tf.placeholder(tf.float32, [None, self.iterations, n_classes])
+        # data ready for training
+        self.valid_data = False
 
 
     def preprocess_tweets(self):
@@ -50,9 +52,57 @@ class RNN(Preprocessor):
         self.one_hot_encode()
 
         print("You're all set to train, woot!")
+        self.data_prepared = True
+
+    def declare_model(self, skip_preprocess=False):
+        if not skip_preprocess:
+            assert self.data_prepared, "Preprocess your data first"
+
+        # isolate training and testing data
+        self.x_train, self.x_test, self.y_train, self.y_test = train_test_split(self.twitter_data.one_hot_encoded.values)
+
+        # initialize placeholders for input and output values
+        self.X = tf.placeholder(tf.float32, [None, self.iterations, self.tweet_size])
+        self.y = tf.placeholder(tf.float32, [None, len(list(self.label_encoder.classes_))])
+        self.input = tf.unstack(self.x, self.timesteps, 1)
+
+        # initialize weights and biases
+
+
+        # define lstm layer
+
+
+        # prediction
+        self.pred = None
+
+        # loss function
+        self.loss = None
+
+        # optimizer
+        self.opt = None
+
+        # evaluate prediction
+
+
+        # check accuracy
+        self.accuracy = None
+
 
     def train(self):
-        pass
+        initializer = tf.global_variables_initializer()
+        with tf.Session() as sess:
+            sess.run(initializer)
+            i = 1
+            train_data = zip(self.x_train, self.y_train)
+            while i < self.iterations:
+                batch_x, batch_y = train_data.next_batch(batch_size=self.batch_size)
+                batch_x = batch_x.reshape((self.batch_size, self.iterations, self.tweet_size))
+                sess.run(self.opt, feed_dict={self.X: batch_x, self.y: batch_y})
+                if i % 50 == 0:
+                    acc = sess.run(self.accuracy, feed_dict={self.x: batch_x, self.y: batch_y})
+                    loss = sess.run(self.loss, feed_dict={self.x: batch_x, self.y: batch_y})
+                    print(i, acc, loss)
+                i += 1
 
     def test(self):
         pass
